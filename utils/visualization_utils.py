19bed8f2007faaa6be67d3f2daff364d374ece01
John Cusack <johncusackccm@gmail.com>
2018-07-19 20:47:19 -0400

    made green box black box for face

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -168,8 +168,10 @@ def draw_bounding_box_on_image(image,
                                   ymin * im_height, ymax * im_height)
   else:
     (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
-  draw.line([(left, top), (left, bottom), (right, bottom),
-             (right, top), (left, top)], width=thickness, fill=color)
+  #draw.line([(left, top), (left, bottom), (right, bottom),
+#             (right, top), (left, top)], width=thickness,
+#             fill=color)
+  draw.rectangle([(left, top),(right, bottom)], fill = 0)
   try:
     font = ImageFont.truetype('arial.ttf', 24)
   except IOError:
@@ -182,24 +184,29 @@ def draw_bounding_box_on_image(image,
   # Each display_str has a top and bottom margin of 0.05x.
   total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
 
-  if top > total_display_str_height:
-    text_bottom = top
-  else:
-    text_bottom = bottom + total_display_str_height
-  # Reverse list and print from bottom to top.
-  for display_str in display_str_list[::-1]:
-    text_width, text_height = font.getsize(display_str)
-    margin = np.ceil(0.05 * text_height)
-    draw.rectangle(
-        [(left, text_bottom - text_height - 2 * margin), (left + text_width,
-                                                          text_bottom)],
-        fill=color)
-    draw.text(
-        (left + margin, text_bottom - text_height - margin),
-        display_str,
-        fill='black',
-        font=font)
-    text_bottom -= text_height - 2 * margin
+  # if top > total_display_str_height:
+  #   text_bottom = top
+  # else:
+  #   text_bottom = bottom + total_display_str_height
+  # # Reverse list and print from bottom to top.
+  # for display_str in display_str_list[::-1]:
+  #   text_width, text_height = font.getsize(display_str)
+  #   margin = np.ceil(0.05 * text_height)
+  #   draw.rectangle(
+  #       [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+  #                                                         text_bottom)],
+  #       fill=color)
+  #
+  #
+  #   draw.text(
+  #       (left + margin, text_bottom - text_height - margin),
+  #       display_str,
+  #       fill='black',
+  #
+  #       font=font)
+  #   text_bottom -= text_height - 2 * margin
+
+
 
 
 def draw_bounding_boxes_on_image_array(image,
